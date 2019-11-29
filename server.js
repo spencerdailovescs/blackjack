c6252fccd37219a0a03f6e63d7446d7726f7b551
willkendall01 <wpk235@gmail.com>
2019-11-29 13:35:43 -0600

    Init express

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
-const http = require('http');
-// require ('./app.js');
-const fs = require('fs')
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-fs.readFile('index.html', function(err,html) {
-	if (err) {
-		console.log("Error reading index.html")
-		throw err;
-	}
-	http.createServer(function(req, res) {
-		res.writeHeader(200, {"Content-Type" : "text/html"});
-		res.write(html);
-		res.end()
-	}).listen(port)
-})
+const express = require('express');
+
+const app = express();
+
+// serve files from the public directory
+app.use(express.static('public'));
+
+// start the express web server listening on 8080
+app.listen(8080, () => {
+  console.log('listening on 8080');
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+//*********** node stuff
+// const http = require('http');
+// // require ('./app.js');
+// const fs = require('fs')
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// fs.readFile('index.html', function(err,html) {
+// 	if (err) {
+// 		console.log("Error reading index.html")
+// 		throw err;
+// 	}
+// 	http.createServer(function(req, res) {
+// 		res.writeHeader(200, {"Content-Type" : "text/html"});
+// 		res.write(html);
+// 		res.end()
+// 	}).listen(port)
+// })
 
 
 
@@ -28,3 +49,5 @@ fs.readFile('index.html', function(err,html) {
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
+
+
